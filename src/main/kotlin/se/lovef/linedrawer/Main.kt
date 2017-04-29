004594fdb668374c81c4d553ec6f71f7af733f3d
Love F <lovef.code@gmail.com>
2017-04-29 19:38:31 +0200

    Exit on escape

--- src/main/kotlin/se/lovef/linedrawer/Main.kt
+++ src/main/kotlin/se/lovef/linedrawer/Main.kt
@@ -2,10 +2,12 @@ package se.lovef.linedrawer
 
 import com.sun.javafx.geom.Vec2d
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.image.Image
+import javafx.scene.input.KeyCode
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.Pane
@@ -43,13 +45,20 @@ class Main : Application() {
                     "#A000C0")
                     .reversed()
                     .forEach { color ->
-                graphicsContext.draw(Color.web(color), polygonLevelIterator.next())
-            }
+                        graphicsContext.draw(Color.web(color), polygonLevelIterator.next())
+                    }
 
             this.scene = Scene(Pane().apply {
                 background = Background(BackgroundFill(Color.BLACK, null, null))
                 children.add(canvas)
-            })
+            }).apply {
+                setOnKeyReleased {
+                    when (it.code) {
+                        KeyCode.ESCAPE -> Platform.exit()
+                        else -> { }
+                    }
+                }
+            }
         }.show()
     }
 
